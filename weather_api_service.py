7e06c2af2f1aeaa9e5d6c81cd729c96ef7b10774
Egor Porofiev <2vubj32cz@gmail.com>
2025-08-03 23:53:21 +0300

    add more parameters

--- weather_api_service.py
+++ weather_api_service.py
@@ -25,9 +25,12 @@ class WeatherType(str, Enum):
 @dataclass(slots=True, frozen=True)
 class Weather:
     temperature: Celsius
+    feels_like_temperature: Celsius
     weather_type: WeatherType
     sunrise: datetime
     sunset: datetime
+    humidity: int
+    wind_speed: int
     city: str
 
 
@@ -54,15 +57,22 @@ def _get_api_response(latitude: float, longitude: float) -> dict:
 def _parse_data(api_data: dict) -> Weather:
     return Weather(
     temperature=_parse_weather_temperature(api_data),
+    feels_like_temperature=_parse_feels_like_temperature(api_data),
     weather_type=_parse_weather_type(api_data),
     sunrise=_parse_sun_time(api_data, "sunrise"),
     sunset=_parse_sun_time(api_data, "sunset"),
+    humidity=_parse_weather_humidity(api_data),
+    wind_speed=_parse_wind_speed(api_data),
     city=_parse_city(api_data)
     )
 
 
 def _parse_weather_temperature(api_data: dict) -> Celsius:
     return round(api_data["main"]["temp"])
+
+
+def _parse_feels_like_temperature(api_data: dict) -> Celsius:
+    return round(api_data["main"]["feels_like"])
 
 
 def _parse_weather_type(data_weather: dict) -> WeatherType:
@@ -90,6 +100,14 @@ def _parse_weather_type(data_weather: dict) -> WeatherType:
     
 def _parse_sun_time(api_data: dict, time: Literal["sunrise"] | Literal["sunset"]) -> datetime:
     return datetime.fromtimestamp(api_data["sys"][time])
+
+
+def _parse_weather_humidity(api_data: dict) -> int:
+    return api_data["main"]["humidity"]
+
+
+def _parse_wind_speed(api_data: dict) -> int:
+    return api_data["wind"]["speed"]
 
 
 def _parse_city(api_data: dict) -> str:
--- weather_formatter.py
+++ weather_formatter.py
@@ -2,8 +2,8 @@ from weather_api_service import Weather
 
 def format_weather(weather: Weather) -> str:
     """Formats weather data in string"""
-    return (f"{weather.city}, Tемпература {weather.temperature}°C, "
-            f"{weather.weather_type.value}\n"
+    return (f"{weather.city}, Tемпература {weather.temperature}°C, ощущается как {weather.feels_like_temperature}°C\n"
+            f"{weather.weather_type.value}, вложность {weather.humidity}%, скорость ветра {weather.wind_speed} м/с\n"
             f"Восход: {weather.sunrise.strftime('%H:%M')}\n"
             f"Закат: {weather.sunset.strftime('%H:%M')}\n")
 
@@ -14,8 +14,11 @@ if __name__ == '__main__':
 
     print(format_weather(Weather(
         temperature=25,
+        feels_like_temperature=24,
         weather_type=WeatherType.CLEAR,
         sunrise=datetime.fromisoformat("2000-01-01 08:01:01"),
         sunset=datetime.fromisoformat("2000-01-01 20:25:00"),
+        humidity=79,
+        wind_speed=6,
         city="Saint-Petersburg"
     )))
